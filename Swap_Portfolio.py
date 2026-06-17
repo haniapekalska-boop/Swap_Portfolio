2e1bb5f1a525f38d2ff8128829c29f0cecb4e22a
Hania <hanipekalska@gmail.com>
2026-06-17 20:48:39 +0200

    Fixed issues with next payment date, added Remove closed position function

--- Swap_Portfolio.py
+++ Swap_Portfolio.py
@@ -42,26 +42,28 @@ class FixedLeg:
         else:
             raise ValueError("Year Accounting Basis must be either 360, 365 or ACT!")
 
-    def next_payment_date(self):#to fix next payment date
-        if self.accounting_basis_month.upper == "03M":
+    def next_payment_date(self):
+        frequency = str(self.payment_frequency.upper()).strip().upper()
+        if frequency in ["03M", "3M"]:
             payment_date = self.dated_date + relativedelta(months=3)
             return payment_date
-        elif self.accounting_basis_month.upper == "06M":
+        elif frequency in ["06M", "6M"]:
             payment_date = self.dated_date + relativedelta(months=6)
             return payment_date
-        elif self.accounting_basis_month.upper == "12M":
+        elif frequency in ["12M", "012M"]:
             payment_date = self.dated_date + relativedelta(months=12)
             return payment_date
         else:
             raise ValueError("Incorrect payment frequency!")
 
-
     def __str__(self):
         return (
             f"FIXED: Notional:{self.notional}, Rate: {self.rate}, "
-            f"Currency: {self.payment_frequency.upper()}, "
-            f"Accounting basis: {self.accounting_basis_year.upper()}/{self.accounting_basis_year.upper()}, "
-            f"Dated date: {self.dated_date}")
+            f"Currency: {self.payment_frequency.upper()},\n "
+            f"\tAccounting basis: {self.accounting_basis_month.upper()}/{self.accounting_basis_year.upper()}, "
+            f"Dated date: {self.dated_date}, "
+            f"Accrued interest: {self.accrued_interest()} {self.currency.upper()}, "
+            f"Next payment date: {self.next_payment_date()}")
 
 
 class FloatingLeg:
@@ -95,12 +97,28 @@ class FloatingLeg:
         else:
             raise ValueError("Year Accounting Basis must be either 360, 365 or ACT!")
 
+    def next_payment_date(self):
+        frequency = str(self.payment_frequency.upper()).strip().upper()
+        if frequency in ["03M", "3M"]:
+            payment_date = self.dated_date + relativedelta(months=3)
+            return payment_date
+        elif frequency in ["06M", "6M"]:
+            payment_date = self.dated_date + relativedelta(months=6)
+            return payment_date
+        elif frequency in ["12M", "012M"]:
+            payment_date = self.dated_date + relativedelta(months=12)
+            return payment_date
+        else:
+            raise ValueError("Incorrect payment frequency!")
+
     def __str__(self):
         return (
             f"FLOATING: Notional:{self.notional}, Rate: {self.rate.upper()}, "
-            f"Currency: {self.payment_frequency.upper()}, "
-            f"Accounting basis: {self.accounting_basis_year.upper()}/{self.accounting_basis_year.upper()}, "
-            f"Dated date: {self.dated_date}")
+            f"Currency: {self.payment_frequency.upper()},\n "
+            f"\tAccounting basis: {self.accounting_basis_month.upper()}/{self.accounting_basis_year.upper()},  "
+            f"Dated date: {self.dated_date}, "
+            f"Accrued interest: {self.accrued_interest()} {self.currency.upper()}, "
+            f"Next payment date: {self.next_payment_date()}")
 
 
 class EventLeg:
@@ -142,23 +160,23 @@ class Swap:
             f"Swap Type: {self.swap_type.upper()} "
             f"Swap ID: {self.swap_id}, "
             f"Maturity date: {self.maturity_date}\n "
-            f"Receivable leg:\n\t {self.receivable_leg}\n "
-            f"Payable leg:\n\t {self.payable_leg} ")
+            f"* Receivable leg:\n\t {self.receivable_leg}\n "
+            f"* Payable leg:\n\t {self.payable_leg} ")
 
     def trade(self, direction: str, amount):
         if direction.upper() == "BUY":
             self.receivable_leg.notional += amount
             self.payable_leg.notional += amount
-            print(f"\nPosition has been increased successfully by {amount}")
+            print(f"\nPosition {self.swap_id} has been increased successfully by {amount}")
         elif direction.upper() == "SELL":
             self.receivable_leg.notional -= amount
             self.payable_leg.notional -= amount
             if self.receivable_leg.notional <= 0 or self.payable_leg.notional <= 0:
                 self.receivable_leg.notional = 0
                 self.payable_leg.notional = 0
-                print("\nPosition has been fully closed\n")
+                print(f"\nPosition {self.swap_id} has been fully closed\n")
             else:
-                print(f"\nPosition has been decreased successfully by {amount}")
+                print(f"\nPosition {self.swap_id} has been decreased successfully by {amount}")
         else:
             raise ValueError("Direction must be either BUY or SELL")
 
@@ -169,7 +187,7 @@ class Portfolio:
 
     def add_swap(self, position):
         self.positions.append(position)
-        print("Position added successfully.")
+        print(f"Position {position.swap_id} added successfully.")
 
     def trade(self, swap_id, direction, amount):
         for position in self.positions:
@@ -178,38 +196,57 @@ class Portfolio:
                 return
         raise ValueError(f"{swap_id} not found")
 
+    def remove_closed_positions(self):
+        initial_count = len(self.positions)
+        self.positions = [position for position in self.positions if position.receivable_leg.notional > 0]
+        removed_count = initial_count - len(self.positions)
+        print(f"Closed positions removed successfully. Removed: {removed_count}")
+
     def __str__(self):
         print("\n**********PORTFOLIO**********\n")
         return "\n\n".join(str(position) for position in self.positions)
 
 
-leg1_irs = FloatingLeg(1000000, "sofr", "usd", "03M",
-                       "act", "365", "2025.12.25")
-leg2_irs = FixedLeg(1000000, 0.01, "usd", "03m",
-                    "30", "ACT", "2025.09.25")
-
-swap1_irs = Swap(swap_type="IRS", maturity_date="2026.06.25", payable_leg=leg1_irs, receivable_leg=leg2_irs,
-                 swap_id="IRS001")
-
-leg1_cds = EventLeg(20000, "RED_CODE001", "EUR")
-leg2_cds = FixedLeg(20000, 0.05, "EUR", "03M",
-                    "30", "ACT", "2026.03.20")
-
-swap1_cds = Swap("CDS", "CDS001", leg2_cds, leg1_cds, "2028.09.20")
-
-leg1_cys = FixedLeg(6000000, 0.025, "eur", "06M",
-                    "act", "act", "2025.10.15")
-leg2_cys = FloatingLeg(750000, "fedl", "usd", "03M",
-                       "30", "360", "2025.04.15")
-
-swap1_cys = Swap("CYS", "CYS001", leg1_cys, leg2_cys, "2060.04.15")
-
-leg1_trs = ReturnLeg(36, "asset001", "EUR")
-leg2_trs = FixedLeg(700000, 0.05, "EUR", "06M",
-                    "30", "360", "2026.02.10")
-swap1_trs = Swap("TRS", "TRS001", leg2_trs, leg1_trs, "2026.08.10")
-
 if __name__ == "__main__":
-    print(leg2_irs.accrued_interest())
-    print(leg2_irs.next_payment_date())
-    print(leg1_irs.accrued_interest())
+    portfolio = Portfolio()
+
+    leg1_irs = FloatingLeg(1000000, "sofr", "usd", "03M",
+                           "act", "365", "2025.11.30")
+    leg2_irs = FixedLeg(1000000, 0.01, "usd", "03M",
+                        "30", "ACT", "2025.09.25")
+
+    swap1_irs = Swap(swap_type="IRS", maturity_date="2026.06.25", payable_leg=leg1_irs, receivable_leg=leg2_irs,
+                     swap_id="IRS001")
+
+    leg1_cds = EventLeg(20000, "RED_CODE001", "EUR")
+    leg2_cds = FixedLeg(20000, 0.05, "EUR", "03M",
+                        "30", "ACT", "2026.03.20")
+
+    swap1_cds = Swap("CDS", "CDS001", leg2_cds, leg1_cds, "2028.09.20")
+
+    leg1_cys = FixedLeg(6000000, 0.025, "eur", "06M",
+                        "act", "act", "2025.10.15")
+    leg2_cys = FloatingLeg(750000, "fedl", "usd", "03M",
+                           "30", "360", "2025.04.15")
+
+    swap1_cys = Swap("CYS", "CYS001", leg1_cys, leg2_cys, "2060.04.15")
+
+    leg1_trs = ReturnLeg(36, "asset001", "EUR")
+    leg2_trs = FixedLeg(700000, 0.05, "EUR", "06M",
+                        "30", "360", "2026.02.10")
+    swap1_trs = Swap("TRS", "TRS001", leg2_trs, leg1_trs, "2026.08.10")
+
+    portfolio.add_swap(swap1_irs)
+    portfolio.add_swap(swap1_cds)
+    portfolio.add_swap(swap1_cys)
+    portfolio.add_swap(swap1_trs)
+
+    print(portfolio)
+
+    swap1_cds.trade("sell", 20000)
+
+    print(portfolio)
+
+    portfolio.remove_closed_positions()
+
+    print(portfolio)
